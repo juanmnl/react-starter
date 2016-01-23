e60b6cb73bb47676135ca2868ad1f8eddc5fd9c3
juanmnl <juanmnlcornejo@gmail.com>
2016-01-22 19:23:37 -0500

    Fix index.html title, remove webpack-html-template, add TODO

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -6,9 +6,9 @@ const Hello = React.createClass({
     return (
       <div>
         <img className="juanmnl-img" />
-        <h1>Can you feel me?</h1>
+        <h1><Icon icon="music" /> Can you feel me?<Icon icon="music" /></h1>
         <h2>Yet another React-based Frontend DevKit</h2>
-        <p>A boilerplate for lightning-fast prototyping with Npm3 + Webpack + Babel + Eslint + Mocha + Sass + React & Bootstrap.</p>
+        <p>A boilerplate for lightning-fast prototyping with Npm + Webpack + Babel + Eslint + Mocha + Sass + React & Bootstrap.</p>
         <hr />
         <p>Made with <Icon icon="heart" /> by
           <a href="http://www.juanmnl.com" target="_blank"> {this.props.name} </a>
