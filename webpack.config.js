9ee2d5a6117dddf65ea20a3dab18e11b069a0d41
juanmnl <juanmnlcornejo@gmail.com>
2016-03-04 16:05:41 -0500

    Fix webpack output path and mod build script

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const precss = require('precss');
 const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   src: path.join(__dirname, 'src'),
-  build: path.join(__dirname, 'dist')
+  dist: path.join(__dirname, 'dist')
 }
 
 process.env.BABEL_ENV = TARGET;
@@ -22,7 +22,7 @@ var common = {
     extensions: ['', '.js', '.jsx']
   },
   output: {
-    path: PATHS.build,
+    path: PATHS.dist,
     filename: 'bundle.js'
   },
   module: {
