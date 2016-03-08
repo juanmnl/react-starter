353d3e151ecd1f91c9a63cb45d0d50912fb8ce55
juanmnl <juanmnlcornejo@gmail.com>
2016-03-08 11:04:25 -0500

    Fix icon v-align

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Icon = (props) => {
   let icon = `fa fa-${props.icon}`;
   return (
-    <i className={icon}></i>
+    <span className='icon'>
+      <i className={icon}></i>
+    </span>
   )
 }
 export default Icon;
